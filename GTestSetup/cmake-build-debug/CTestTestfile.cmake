b6dc006ef2285f77b3614071128198fa6159308b
Alexandre Brown <alexandre.brown@microdrones.com>
2021-01-26 01:43:52 -0500

    Removed cmake-build-debug

--- GTestSetup/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/brownthedev/Documents/GTestSetupUsingExternalProject/GTestSetup
-# Build directory: /home/brownthedev/Documents/GTestSetupUsingExternalProject/GTestSetup/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src/test")
--- GTestSetup/cmake-build-debug/src/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/brownthedev/Documents/GTestSetupUsingExternalProject/GTestSetup/src/test
-# Build directory: /home/brownthedev/Documents/GTestSetupUsingExternalProject/GTestSetup/cmake-build-debug/src/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(GTestSetup_test "GTestSetup_test")
-set_tests_properties(GTestSetup_test PROPERTIES  _BACKTRACE_TRIPLES "/home/brownthedev/Documents/GTestSetupUsingExternalProject/GTestSetup/src/test/CMakeLists.txt;5;add_test;/home/brownthedev/Documents/GTestSetupUsingExternalProject/GTestSetup/src/test/CMakeLists.txt;0;")
